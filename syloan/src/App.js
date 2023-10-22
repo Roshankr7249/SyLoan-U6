374db8864bb6d5ba22c1e91fbdb22b6f24902d90
kumod1164 <kumodsharma1164@gmail.com>
2023-10-22 13:33:57 +0530

    Reset to BoilerPlate

--- syloan/src/App.js
+++ syloan/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 import Signup from './Components/Signup';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">      
         <Signup/>
     </div>
   );
--- syloan/src/Components/Signup.jsx
+++ syloan/src/Components/Signup.jsx
@@ -4,12 +4,7 @@ import styles from "../Components/Signup.module.css"
 const Signup = () => {
   return (
     <div id={styles.container}>
-        <div className={styles.left}>
-            <h1>Left Part</h1>
-        </div>
-        <div className={styles.right}>
-            <h1>Right Part</h1>
-        </div>
+      
     </div>
     
   )
