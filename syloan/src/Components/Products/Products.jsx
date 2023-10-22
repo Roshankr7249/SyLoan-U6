b7acf887be317d5afa1ff7326bf6a7019c14982e
kumod1164 <kumodsharma1164@gmail.com>
2023-10-22 14:26:36 +0530

    card is created

---
+++ syloan/src/Components/Products/Products.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import Styles from "../Products/Products.module.css"
+import cerditCard from "./productsImages/creditCard.png"
+const Products = () => {
+  return (
+    <div>
+        <h2>Products</h2>
+        <div id={Styles.container}>
+            <div className={Styles.card}>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>CREDIT CARDS</h4>
+                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>Get Best Offers →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Products
