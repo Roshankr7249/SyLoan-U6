4b2643343e2227070ee8c4e8ef09e5a3fe82aacb
kumod1164 <kumodsharma1164@gmail.com>
2023-10-23 07:54:49 +0530

    Description Added

--- syloan/src/Components/Products/Products.jsx
+++ syloan/src/Components/Products/Products.jsx
@@ -10,88 +10,103 @@ const Products = () => {
                 <div className={Styles.card1}>
                     <div className={Styles.info}>
                         <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <p>From 35+ options, choose a card matching your lifestyle & needs</p>
                         <p>Get Best Offers →</p>
                     </div>
                     <div>
                         <img src={cerditCard} alt="" />
                     </div>
                 </div>
+
                 <div className={Styles.card1}>
                     <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
+                        <h4>PERSONAL LOAN</h4>
+                        <p>Select the best offer curated just for you from a wide choice of Banks & NBFC's</p>
+                        <p>Check Eligibilty →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+
+
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>MICRO LOANS </h4>
+                        <p>Instant small ticket loans to meet your immediate cash needs</p>
+                        <p>Get Instant Loans →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+
+
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>BUSINESS LOAN (Under 50K)</h4>
+                        <p>Expand your business with loans at low interest rates</p>
+                        <p>Check Eligibility →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+
+
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>TRANSFER PERSONAL LOAN</h4>
+                        <p>Get better interest rates on your existing personal loan</p>
+                        <p>Reduce Your EMI →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+
+
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>HOME LOAN</h4>
+                        <p>Choose from lowest interest rates available for your dream home</p>
+                        <p>Check Eligibility →</p>
+                    </div>
+                    <div>
+                        <img src={cerditCard} alt="" />
+                    </div>
+                </div>
+
+
+                <div className={Styles.card1}>
+                    <div className={Styles.info}>
+                        <h4>LOAN AGAINST PROPERTY</h4>
+                        <p>Get liquidity against your property at best interest rates</p>
                         <p>Get Best Offers →</p>
                     </div>
                     <div>
                         <img src={cerditCard} alt="" />
                     </div>
                 </div>
+
+
                 <div className={Styles.card1}>
                     <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
-                        <p>Get Best Offers →</p>
+                        <h4>TRANSFER HOME LOAN</h4>
+                        <p>Get better interest rates on your existing home loan</p>
+                        <p>Reduce Your EMI →</p>
                     </div>
                     <div>
                         <img src={cerditCard} alt="" />
                     </div>
                 </div>
+
+
                 <div className={Styles.card1}>
                     <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
-                        <p>Get Best Offers →</p>
-                    </div>
-                    <div>
-                        <img src={cerditCard} alt="" />
-                    </div>
-                </div>
-                <div className={Styles.card1}>
-                    <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
-                        <p>Get Best Offers →</p>
-                    </div>
-                    <div>
-                        <img src={cerditCard} alt="" />
-                    </div>
-                </div>
-                <div className={Styles.card1}>
-                    <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
-                        <p>Get Best Offers →</p>
-                    </div>
-                    <div>
-                        <img src={cerditCard} alt="" />
-                    </div>
-                </div>
-                <div className={Styles.card1}>
-                    <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
-                        <p>Get Best Offers →</p>
-                    </div>
-                    <div>
-                        <img src={cerditCard} alt="" />
-                    </div>
-                </div>
-                <div className={Styles.card1}>
-                    <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
-                        <p>Get Best Offers →</p>
-                    </div>
-                    <div>
-                        <img src={cerditCard} alt="" />
-                    </div>
-                </div>
-                <div className={Styles.card1}>
-                    <div className={Styles.info}>
-                        <h4>CREDIT CARDS</h4>
-                        <p>From 35+ options, choos Lorem ipsum dolor sit amet, cons dascs sdv</p>
-                        <p>Get Best Offers →</p>
+                        <h4>STEP UP CREDIT CARDS</h4>
+                        <p>A Credit Card for everyone, backed by a Fixed Deposit</p>
+                        <p>Know More →</p>
                     </div>
                     <div>
                         <img src={cerditCard} alt="" />
