fd388cf92ea9b02c872bf1e5f49130ba8df95301
kumod1164 <kumodsharma1164@gmail.com>
2023-10-24 20:15:59 +0530

    For mobile verification

--- syloan/src/App.js
+++ syloan/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MobileVerification from './Components/MobileVerification/MobileVerification';
 import Products from './Components/Products/Products';
 
 function App() {
   return (
     <div className="App">
       <Products/>
+      {/* <MobileVerification/> */}
     </div>
   );
 }
